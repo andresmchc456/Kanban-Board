7bfe70d5bb9648bc81920b5392c9348f216f53eb
Andrés Meléndez <andresmchc456@gmail.com>
2026-07-03 12:28:13 -0500

    feat: integrate Firestore

--- src/app/services/kanban.ts
+++ src/app/services/kanban.ts
@@ -1,5 +1,5 @@
 import { inject, Injectable } from '@angular/core';
-import { Firestore, collection, collectionData, addDoc } from '@angular/fire/firestore';
+import { Firestore, collection, collectionData, addDoc, doc, updateDoc } from '@angular/fire/firestore'; // Importaciones de Firestore 
 import { Observable } from 'rxjs';
 
 export interface Task { // interface define la estructura de los datos
@@ -15,6 +15,8 @@ export class KanbanService { // exporta la clase para que pueda ser utilizada en
   private firestore = inject(Firestore); // injecta el servicio de Firestore
   private taskCollection = collection(this.firestore, 'tasks'); // crea una referencia a la coleccion 'tasks'
 
+  //FUNCIONES 
+
   // Obtener tareas en tiempo real
   getTasks(): Observable<Task[]> {
     return collectionData(this.taskCollection, { idField: 'id' }) as Observable<Task[]>;
@@ -23,5 +25,10 @@ export class KanbanService { // exporta la clase para que pueda ser utilizada en
   // agrega una tarea 
   addTask(task: Task) {
     return addDoc(this.taskCollection, task);
+  }
+
+  updateTaskStatus(taskId: string, newStatus: 'todo' | 'doing' | 'done') {
+    const taskDocRef = doc(this.firestore, `tasks/${taskId}`);
+    return updateDoc(taskDocRef, { status: newStatus });
   }
 } 
