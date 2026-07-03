e87004f2c090899d1d81af5d1cd7084278dc5a00
Andrés Meléndez <andresmchc456@gmail.com>
2026-07-03 12:13:18 -0500

    componentes conectados con firestore

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,25 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { KanbanService, Task } from './services/kanban';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('kanban-firebase');
+
+export class AppComponent implements OnInit {
+  title = 'kanban-firebase';
+  // Inyectamos nuestro servicio Kanban
+  private KanbanServises = inject(KanbanService);
+  // Creamos el Observable para las tareas
+  tasks$!: Observable<Task[]>;
+
+  ngOnInit(): void {
+    this.tasks$ = this.KanbanServises.getTasks();// Obtenemos las tareas en tiempo real al cargar el componente
+
+  }
 }
